f025377c74c00cb9b372f4fcc485d27d7796544d
lionelsu <lionel_suicune@hotmail.com>
2023-02-26 16:46:48 -0400

    req: added functions to hangle with change border style

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const memeText = document.querySelector('#meme-text');
 const imageInsert = document.querySelector('#meme-insert');
 const memeImage = document.querySelector('#meme-image');
 
+const btnContainer = document.querySelector('#border-button-container');
+
 function createText() {
   memeText.textContent = inputText.value;
 }
@@ -12,3 +14,24 @@ function loadImage(event) {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
 }
 imageInsert.addEventListener('change', loadImage);
+
+function getStyle(id, value) {
+  const elementId = document.querySelector(id);
+  const computedStyle = window.getComputedStyle(elementId);
+  const style = computedStyle.getPropertyValue(value);
+
+  return style;
+}
+
+function changeStyle(id, value) {
+  const memeContainer = document.querySelector('#meme-image-container');
+  const styleValue = getStyle(id, value);
+  memeContainer.style[value] = styleValue;
+}
+
+btnContainer.addEventListener('click', (event) => {
+  const btnId = event.target.id;
+  if (btnId === 'fire' || btnId === 'water' || btnId === 'earth') {
+    changeStyle(`#${btnId}`, 'border');
+  }
+});
