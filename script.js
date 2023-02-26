efa90f637fbcece0bb818e73f5e8ab0abdf69370
lionelsu <lionel_suicune@hotmail.com>
2023-02-26 15:02:49 -0400

    req: 2, 3 & 4 done: added functions to read image and meme text

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const inputText = document.querySelector('#text-input');
-function selectorFactory() {
-  const memeText = document.querySelector('#meme-text');
-  return {
-    memeText,
-  };
-}
+const memeText = document.querySelector('#meme-text');
+const imageInsert = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
 
 function createText() {
-  const { memeText } = selectorFactory();
   memeText.textContent = inputText.value;
 }
 inputText.addEventListener('input', createText);
+
+function loadImage(event) {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+}
+imageInsert.addEventListener('change', loadImage);
